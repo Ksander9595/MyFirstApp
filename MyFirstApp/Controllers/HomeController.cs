873f6cea50a189c7e5de901b75f2da9e5156f2be
Tommy <84313548+Ksander9595@users.noreply.github.com>
2023-12-11 16:48:50 +0700

    Added"Edit"

--- MyFirstApp/Controllers/HomeController.cs
+++ MyFirstApp/Controllers/HomeController.cs
@@ -33,5 +33,38 @@ namespace MyFirstApp.Controllers
             await db.SaveChangesAsync();
             return RedirectToAction("Index");
         }
+        [HttpPost]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id != null)
+            {
+                //User user = new User { Id = id.Value };
+                //db.Entry(user).State = EntityState.Deleted;
+                User? user = await db.Users.FirstOrDefaultAsync(p => p.Id == id);
+                if (user != null)
+                {
+                    db.Users.Remove(user);
+                    await db.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+            }
+            return NotFound();
+        }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != null)
+            {
+                User? user = await db.Users.FirstOrDefaultAsync(p=>p.Id == id);
+                if (user != null) return View(user);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(User user)
+        {
+            db.Users.Update(user);
+            await db.SaveChangesAsync();    
+            return RedirectToAction("Index");
+        }
     }
 }
